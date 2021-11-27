f78ea920d1c0a3e481e7e12337c5259ffe43d6e2
Sergey Dushin <s.dushin@inbox.ru>
2021-11-27 14:11:44 +0300

    create necessary classes and files

---
+++ MIMO_OFDM_system_main.m
@@ -0,0 +1,42 @@
+% =====================================================================    
+% MIMO-OFDM implemetation for hydroacoustic link 
+% ===================================================================== 
+
+% paths
+clear; close all;
+addpath('./External-libs/Watermark-dataset/BCH1/mat');
+addpath('./External-libs/Watermark-dataset/matlab');
+addpath('./External-libs/Nissel-FBMC-OFDM');
+addpath('./External-libs/Nissel-FBMC-OFDM/Theory');
+
+
+% ======================================================================
+% Simulation parameters   
+%=======================================================================
+% transmited data
+% rx_tx common parameters
+% transmitter parameters
+% receiver prarameters
+% validate parameters 
+tx_params =1
+
+% ======================================================================
+% Create objects   
+%======================================================================+
+% transmitter
+% channel
+% receiver
+
+
+
+% ======================================================================
+% Run simulation   
+%=======================================================================
+
+%transmit signal
+%apply channel 
+%receive signal
+%estimate the perfomznce
+
+
+
